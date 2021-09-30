644a36f6f19401e5270f27a735c4a3074964e605
agy2021 <83178990+agy2021@users.noreply.github.com>
2021-09-30 09:02:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,30 +10,5 @@ while True:
         os.system("clear")
         exit()
     else:
-        if "code" in app or "ide" in app:
-            os.system('open -a "visual studio code"')
-            os.system("clear")
-        elif "java" in app:
-            os.system('open -a "intellij idea ce"')
-            os.system("clear")
-        elif "chrome" in app:
-            os.system('open -a "google chrome"')
-            os.system("clear")
-        elif "saf" in app:
-            os.system('open -a safari')
-            os.system("clear")
-        elif "repl" in app:
-            os.system('open -a "replit.com"')
-            os.system("clear")
-        elif "jlearn" in app:
-            os.system('open -a "java learning | mike dane"')
-            os.system("clear")
-        elif "nt" in app:
-            os.system('open -a "nitro type"')
-            os.system("clear")
-        elif "settings" in app:
-            os.system('open -a "system preferences"')
-            os.system("clear")
-
         os.system('open -a "' + app + '"')
         os.system("clear")
